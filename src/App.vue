cca05d9303d3da0d45161bf8a59d9f222f582188
Nick Gashkov <nick.eugene.gashkov@gmail.com>
2018-11-19 23:56:07 +0300

    Add 'bulma' and 'vue-awesome'

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,6 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "~bulma";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
+import "vue-awesome/icons";
+
 import Vue from "vue";
+import Icon from "vue-awesome/components/Icon";
+
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
+Vue.component("v-icon", Icon);
 
 new Vue({
   render: h => h(App)
