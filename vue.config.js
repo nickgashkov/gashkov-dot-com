82af8e17b38567e9d1de4bec610f946cc1f78b76
Nick Gashkov <nick.eugene.gashkov@gmail.com>
2018-11-24 01:30:01 +0300

    Add 's3-deploy' plugin

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,31 @@
 module.exports = {
   lintOnSave: false,
+
   chainWebpack: config => {
     const svgRule = config.module.rule("svg");
 
     svgRule.uses.clear();
     svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+  },
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: "default",
+      region: "us-east-1",
+      bucket: "gashkov.com",
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: "index.html",
+      staticErrorPage: "index.html",
+      assetPath: "dist",
+      assetMatch: "**",
+      deployPath: "/",
+      acl: "public-read",
+      pwa: false,
+      enableCloudfront: false,
+      uploadConcurrency: 5,
+      pluginVersion: "3.0.0"
+    }
   }
 };
